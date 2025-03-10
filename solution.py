16520cacb7e5070b5c97ae3f9c8f55c38df3a7c8
Luís Pereira <54816048+lumafepe@users.noreply.github.com>
2025-03-10 14:38:47 +0000

    Create solution.py

---
+++ solution.py
@@ -0,0 +1,7 @@
+n_palavras = int(intput())
+maior_palavra = ""
+for i in range(n_palavras):
+    palavra = input()
+    if len(palavra) > len(maior_palavra):
+      maior_palavra = palavra
+print(maior_palavra)
